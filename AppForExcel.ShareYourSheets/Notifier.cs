a142a2babede7849d2d1ea17ad86f743677bdeb9
unknown <mickael.metesreau@gmail.com>
2015-02-20 13:28:14 +0100

    Initial commit

---
+++ AppForExcel.ShareYourSheets/Notifier.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForExcel.ShareYourSheets
+{
+    public class Notifier : Hub
+    {
+        private static List<string> users = new List<string>();
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled).ContinueWith((param) =>
+            {
+                if (users.Contains(Context.ConnectionId))
+                {
+                    users.Remove(Context.ConnectionId);
+                    Clients.All.Users(users);
+                }
+            });
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected().ContinueWith((param) =>
+            {
+                users.Add(Context.ConnectionId);
+                Clients.All.Users(users);
+            });
+        }
+
+        public void NotifyUser(string connectionId, string datas)
+        {
+            Clients.Client(connectionId).ReceivedDatas(Context.ConnectionId, datas, DateTime.Now.ToString("dd/MM/yyyy"));
+        }
+    }
+}
